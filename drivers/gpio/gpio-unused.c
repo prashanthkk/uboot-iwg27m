0db526274f4cac991720c8f112735d94f9c93f2f
prashanth <prashanth.kumar@iwavesystems.com>
2020-08-04 18:42:26 +0530

    Updated uboot folders

---
+++ drivers/gpio/gpio-unused.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2020 iWave Systems Technologies Pvt. Ltd.
+ *
+ */
+
+/*
+ * @file gpio-unused.c
+ *
+ * @brief Simple driver to set the unused GPIOs as input
+ *
+ * @ingroup GPIO
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <dt-bindings/gpio/gpio.h>
+#include <errno.h>
+#include <fdtdec.h>
+#include <malloc.h>
+#include <asm-generic/gpio.h>
+#include <linux/bug.h>
+#include <linux/ctype.h>
+
+/* 
+ * iw_gpio_probe - Probe method for the GPIO device.
+ * @np: pointer to device tree node
+ *
+ * This function probes the Unused GPIOs in the device tree. It request GPIOs
+ * as input. It returns 0, if all the GPIOs is requested as input
+ * or a negative value if there is an error.
+ */
+static int iw_gpio_probe(struct udevice *dev)
+{
+	struct gpio_desc *num_gpios;
+	int num_ctrl, ret;
+
+	//printf("\n IWG Inside Unused GPIO probe \n");
+	/* Fill GPIO pin array */
+	num_ctrl = gpio_get_list_count(dev, "gpios");
+	if (num_ctrl > 0) {
+		num_gpios = malloc(sizeof(struct gpio_desc) *num_ctrl);
+#if 1
+	//	printf("\n IWG Inside GPIO num_ctrl \n");
+		ret = gpio_request_list_by_name(dev, "gpios", num_gpios,
+				num_ctrl, GPIOD_IS_IN);
+		if (ret < 0) {
+			dev_err("Can't get %s gpios! Error: %d", dev->name, ret);
+			return ret;
+		}
+#endif
+		free(num_gpios);            
+	} else {
+		dev_err(dev, "gpios DT property empty / missing\n");
+		return -ENODEV;
+	}	
+
+	return 0;
+}
+
+static int iw_gpio_remove(struct udevice *dev)
+{
+	/* Platform not registerd return silently */
+	return 0;
+}
+
+static const struct udevice_id iw_gpio_ids[] = {
+	{ .compatible = "iwave,unused-gpios" },
+	{ }
+};
+
+U_BOOT_DRIVER(iwgpio_unused) = {
+	.name   = "iw_gpio",
+	.id     = UCLASS_GPIO,
+	.probe          = iw_gpio_probe,
+	.of_match 	= iw_gpio_ids,
+	.remove         = iw_gpio_remove,
+};
---
+++ include/configs/imx8qm_iwg27m_uuu.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020 iWave System Technologies Pvt Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __IMX8QM_IWG27M_UUU_H
+#define __IMX8QM_IWG27M_UUU_H
+
+#include "imx8qm_iwg27m.h"
+
+#ifdef BSP_VERSION
+#undef BSP_VERSION
+#define BSP_VERSION	"iW-PRFHZ-SC-01-R3.0-REL1.0-Linux5.4.3_UUU"
+#endif
+
+#endif
+
